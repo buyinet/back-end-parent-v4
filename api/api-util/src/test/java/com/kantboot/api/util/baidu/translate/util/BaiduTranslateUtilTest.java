1f1bbbc47eb0f9e2edf39da77d3685e568cf7971
自由的道德群众 <2453201633@qq.com>
2023-04-10 05:27:50 +0800

    修改了一下README.md和测试

--- api/api-util/src/test/java/com/kantboot/api/util/baidu/translate/util/BaiduTranslateUtilTest.java
+++ api/api-util/src/test/java/com/kantboot/api/util/baidu/translate/util/BaiduTranslateUtilTest.java
@@ -2,6 +2,7 @@ package com.kantboot.api.util.baidu.translate.util;
 
 import com.alibaba.fastjson2.JSON;
 import com.kantboot.api.util.baidu.translate.entity.BaiduTranslateParam;
+import com.kantboot.api.util.baidu.translate.entity.BaiduTranslateResult;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -23,8 +24,10 @@ public class BaiduTranslateUtilTest {
         // 翻译的目标语言，可以使用百度翻译开放平台的语言列表中的编码，后期会扩展也可以使用国际化的语言编码
         baiduTranslateParam.setTo("en");
 
+        // 获取翻译结果
+        BaiduTranslateResult translate = BaiduTranslateUtil.translate(baiduTranslateParam);
         // 输出结果：{"src":"你好","dst":"Hello"}
-        System.out.println(JSON.toJSONString(BaiduTranslateUtil.translate(baiduTranslateParam)));
+        System.out.println(JSON.toJSONString(translate));
     }
 
 }
